c22bceacd5fbc374c0d64cf4efc8b849111d60d9
Rohit Gund <iamrohitgund@gmail.com>
2021-06-17 19:30:46 -0700

    RegEx

---
+++ RegEx/simple.py
@@ -0,0 +1,44 @@
+import re
+
+text_to_search = '''
+abcdefghijklmnopqurtuvwxyz
+ABCDEFGHIJKLMNOPQRSTUVWXYZ
+1234567890
+
+Ha HaHa
+
+MetaCharacters (Need to be escaped):
+. ^ $ * + ? { } [ ] \ | ( )
+
+coreyms.com
+
+321-555-4321
+123.555.1234
+123*555*1234
+800-555-1234
+900-555-1234
+
+Mr. Schafer
+Mr Smith
+Ms Davis
+Mrs. Robinson
+Mr. T
+'''
+
+sentence = 'Start a sentence and then bring it to an end'
+
+pattern = re.compile(r'start', re.I) #re.I is the flag for ignoring the case in regular expression 
+#we are searching with 'start' due to re.I it will match with 'Start' 
+
+matches = pattern.search(sentence)
+
+print(matches)
+
+'''
+with open('data.txt' ,'r', encoding ='utf-8) as f:
+    contents = f.read()
+    matches = pattern.finditer(contents)
+
+    for match in matches:
+        print(match)
+'''
